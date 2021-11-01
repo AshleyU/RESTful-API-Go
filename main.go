424975ab8e640aed07a72f85233902b067ce1195
Ashley U <ashley.umetsu@gmail.com>
2021-11-01 14:19:15 -0500

    Setting up go server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint hit: homePage")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	fmt.Println("Running")
+	handleRequest()
+}
