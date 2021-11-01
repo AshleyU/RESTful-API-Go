783138c6f19eba84a64729216c70152d204d3d1a
Ashley U <ashley.umetsu@gmail.com>
2021-11-01 15:19:48 -0500

    Adding article page

--- main.go
+++ main.go
@@ -4,7 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+//Global array that we can populate in our main function to simulate a database
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles {
+		Article{Title:"Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
+	
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -13,6 +32,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
