b3b0889fb9cbe562ac124036223f3a27f8610f5b
Ashley U <ashley.umetsu@gmail.com>
2021-11-02 11:21:41 -0500

    Adding delete, post, and put requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"encoding/json"
@@ -9,25 +10,93 @@ import (
 )
 
 type Article struct {
+	Id string `json:"Id"`
 	Title string `json:"Title"`
 	Desc string `json:"desc"`
 	Content string `json:"content"`
 }
 
-//Global array that we can populate in our main function to simulate a database
-type Articles []Article
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles {
-		Article{Title:"Title Example", Desc: "Description Example", Content: "Content Example"},
-	}
-	
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllArticles")
+    json.NewEncoder(w).Encode(Articles)
 }
 
-func postArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "POST endpoint worked")
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+    // get the body of our POST request
+    // return the string response containing the request body    
+    reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article 
+    json.Unmarshal(reqBody, &article)
+    // update our global Articles array to include
+    // our new Article
+    Articles = append(Articles, article)
+
+    json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+    // once again, we will need to parse the path parameters
+    vars := mux.Vars(r)
+    // we will need to extract the `id` of the article we
+    // wish to delete
+    id := vars["id"]
+
+    // we then need to loop through all our articles
+    for index, article := range Articles {
+        // if our id path parameter matches one of our
+        // articles
+        if article.Id == id {
+            // updates our Articles array to remove the 
+            // article
+            Articles = append(Articles[:index], Articles[index+1:]...)
+        }
+    }
+
+	fmt.Println("Deleted")
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+    // once again, we will need to parse the path parameters
+    vars := mux.Vars(r)
+    // we will need to extract the `id` of the article we
+    // wish to delete
+    id := vars["id"]
+
+	var updatedEvent Article 
+	
+	reqBody, _ := ioutil.ReadAll(r.Body) 
+	json.Unmarshal(reqBody, &updatedEvent) 
+	for i, article := range Articles { 
+		
+		if article.Id == id {
+
+		article.Title = updatedEvent.Title
+		article.Desc = updatedEvent.Desc
+		article.Content = updatedEvent.Content
+		Articles[i] = article
+		json.NewEncoder(w).Encode(article)
+        }
+    }
+	fmt.Println("Updated")
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    key := vars["id"]
+
+	// Loop over all of our Articles
+    // if the article.Id equals the key we pass in
+    // return the article encoded as JSON
+    for _, article := range Articles {
+        if article.Id == key {
+            json.NewEncoder(w).Encode(article)
+        }
+    }
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -39,12 +108,18 @@ func handleRequest() {
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", postArticles).Methods("POST")
+	myRouter.HandleFunc("/articles", returnAllArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/articles/{id}", updateArticle).Methods("PUT")
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticle).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
-	fmt.Println("Running")
+	Articles = []Article{
+        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
 	handleRequest()
 }
